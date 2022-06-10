3b38ccbf97b617e4ae3fe12207378f9bced1d26f
duy nguyen <npnhatduy@gmail.com>
2022-06-10 16:30:08 +0700

    Commit 2 khoi tao cac Form Identity
    Khoi tao Identity

---
+++ Data/RoleName.cs
@@ -0,0 +1,9 @@
+namespace ClinicManagement.Data
+{
+    public class RoleName 
+    {
+        public const string Administrator = "Administrator";
+        public const string Editor = "Editor";
+        public const string Member = "Member";
+    }
+}
--- Startup.cs
+++ Startup.cs
@@ -6,6 +6,7 @@ using ClinicManagement.Models;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
@@ -29,6 +30,51 @@ namespace ClinicManagement
             services.AddDbContext<ClinicManagementDbContext>(options=>{
                 options.UseSqlServer(Configuration.GetConnectionString("AppDbContext"));
             });
+
+              // Dang ky Identity
+            services.AddIdentity<AppUser, IdentityRole>()
+                    .AddEntityFrameworkStores<ClinicManagementDbContext>()
+                    .AddDefaultTokenProviders();
+
+            // services.AddDefaultIdentity<AppUser>()
+            //         .AddEntityFrameworkStores<MyBlogContext>()
+            //         .AddDefaultTokenProviders();
+                    
+
+            // Truy cập IdentityOptions
+            services.Configure<IdentityOptions> (options => {
+                // Thiết lập về Password
+                options.Password.RequireDigit = false; // Không bắt phải có số
+                options.Password.RequireLowercase = false; // Không bắt phải có chữ thường
+                options.Password.RequireNonAlphanumeric = false; // Không bắt ký tự đặc biệt
+                options.Password.RequireUppercase = false; // Không bắt buộc chữ in
+                options.Password.RequiredLength = 3; // Số ký tự tối thiểu của password
+                options.Password.RequiredUniqueChars = 1; // Số ký tự riêng biệt
+
+                // Cấu hình Lockout - khóa user
+                options.Lockout.DefaultLockoutTimeSpan = TimeSpan.FromMinutes (5); // Khóa 5 phút
+                options.Lockout.MaxFailedAccessAttempts = 3; // Thất bại 3 lầ thì khóa
+                options.Lockout.AllowedForNewUsers = true;
+
+                // Cấu hình về User.
+                options.User.AllowedUserNameCharacters = // các ký tự đặt tên user
+                    "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789-._@+";
+                options.User.RequireUniqueEmail = true;  // Email là duy nhất
+            
+
+                // Cấu hình đăng nhập.
+                options.SignIn.RequireConfirmedEmail = true;            // Cấu hình xác thực địa chỉ email (email phải tồn tại)
+                options.SignIn.RequireConfirmedPhoneNumber = false;     // Xác thực số điện thoại
+                options.SignIn.RequireConfirmedAccount = true; 
+                
+            });      
+
+            services.ConfigureApplicationCookie(options => {
+                options.LoginPath = "/login/";
+                options.LogoutPath = "/logout/";
+                options.AccessDeniedPath = "/khongduoctruycap.html";
+            });  
+
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -49,6 +95,7 @@ namespace ClinicManagement
 
             app.UseRouting();
 
+            app.UseAuthentication();
             app.UseAuthorization();
 
             app.UseEndpoints(endpoints =>
